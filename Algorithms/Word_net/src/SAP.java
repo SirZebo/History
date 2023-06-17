ee5029794b74027ea99eb40b0d740a271710c1d9
SirZebo <123633590+SirZebo@users.noreply.github.com>
2023-06-17 15:48:23 +0800

    Add files via upload

---
+++ Algorithms/Word_net/src/SAP.java
@@ -0,0 +1,165 @@
+import java.util.*;
+
+public class SAP {
+    Digraph G;
+    final Ancestors[] ancestors;
+    Map<Map.Entry<Integer, Integer>, Map.Entry<Integer, Integer>> sapLengthMap;
+
+
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Digraph G) {
+        this.G = G;
+        sapLengthMap = new HashMap<>();
+        this.ancestors = topologicalSort(G);
+    }
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+        if (!sapLengthMap.containsKey(Map.entry(v, w))) {
+            ancestor(v, w);
+        }
+        return sapLengthMap.get(Map.entry(v, w)).getValue();
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+        Deque<Integer> ancestorsV = new ArrayDeque<>(ancestors[v].ancestors);
+        Deque<Integer> ancestorsW = new ArrayDeque<>(ancestors[w].ancestors);
+        int commonAncestor = -1;
+        // Remove queue until ancestors are different
+        while (ancestorsV.peek().equals(ancestorsW.peek())) {
+            commonAncestor = ancestorsV.poll();
+            ancestorsW.poll();
+            if (ancestorsV.isEmpty() || ancestorsW.isEmpty()) {
+                break;
+            }
+        }
+        this.sapLengthMap.put(Map.entry(v, w), Map.entry(commonAncestor, commonAncestor == -1 ? -1 : ancestorsV.size() + ancestorsW.size() + 2));
+        return commonAncestor;
+    }
+
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> vSet, Iterable<Integer> wSet) {
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        for (int v : vSet) {
+            for (int w : wSet) {
+                if (!sapLengthMap.containsKey(Map.entry(v, w))) {
+                    ancestor(v, w);
+                }
+                treeMap.put(sapLengthMap.get(Map.entry(v, w)).getValue(), sapLengthMap.get(Map.entry(v, w)).getKey());
+            }
+        }
+        return treeMap.firstKey();
+    }
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> vSet, Iterable<Integer> wSet) {
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        for (int v : vSet) {
+            for (int w : wSet) {
+                if (!sapLengthMap.containsKey(Map.entry(v, w))) {
+                    ancestor(v, w);
+                }
+                treeMap.put(sapLengthMap.get(Map.entry(v, w)).getValue(), sapLengthMap.get(Map.entry(v, w)).getKey());
+            }
+        }
+        return treeMap.firstEntry().getValue();
+    }
+
+    private Ancestors[] topologicalSort(Digraph G) {
+        int[] inDegrees = new int[G.V()];
+        Queue<Integer> queue = new LinkedList<>();
+        Ancestors[] ancestors = new Ancestors[G.V()];
+
+        // Get outDegrees[]
+        for (int i = 0; i < G.V(); i++) {
+            inDegrees[i] = G.indegree(i);
+        }
+
+        for (int i = 0; i < inDegrees.length; i++) {
+            if (inDegrees[i] == 0) {
+                queue.add(i);
+                ancestors[i] = new Ancestors(new ArrayDeque<>());
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                int curr = queue.poll();
+                for (int next : G.adj(curr)) {
+                    inDegrees[next]--;
+                    if (inDegrees[next] == 0) {
+                        queue.add(next);
+                        ancestors[next] = new Ancestors(new ArrayDeque<>(ancestors[curr].ancestors));
+                        ancestors[next].addAncestor(curr);
+                    }
+                }
+            }
+        }
+        return ancestors;
+    }
+
+    class Ancestors {
+        Deque<Integer> ancestors;
+
+        public Ancestors(Deque<Integer> ancestors) {
+            this.ancestors = ancestors;
+        }
+
+        public void addAncestor(int ancestor) {
+            ancestors.add(ancestor);
+        }
+    }
+
+    // do unit testing of this class
+    public static void main(String[] args) {
+
+        Digraph G = new Digraph(25);
+        G.addEdge(1, 0);
+        G.addEdge(2, 0);
+        G.addEdge(3, 1);
+        G.addEdge(4, 1);
+        G.addEdge(5, 2);
+        G.addEdge(6, 2);
+        G.addEdge(10, 5);
+        G.addEdge(11, 5);
+        G.addEdge(12, 5);
+        G.addEdge(17, 10);
+        G.addEdge(18, 10);
+        G.addEdge(19, 12);
+        G.addEdge(20, 12);
+        G.addEdge(23, 20);
+        G.addEdge(24, 20);
+        G.addEdge(7, 3);
+        G.addEdge(8, 3);
+        G.addEdge(9, 3);
+        G.addEdge(13, 7);
+        G.addEdge(14, 7);
+        G.addEdge(15, 9);
+        G.addEdge(16, 9);
+        G.addEdge(21, 16);
+        G.addEdge(22, 16);
+
+
+        G = G.reverse();
+        SAP sap = new SAP(G);
+        List<Integer> v = new ArrayList<>();
+        v.add(13);
+        v.add(23);
+        v.add(24);
+        List<Integer> w = new ArrayList<>();
+        w.add(6);
+        w.add(16);
+        w.add(17);
+
+        int commonAncestor = sap.ancestor(v, w);
+        int sapLength = sap.length(v, w);
+        System.out.println("length: " + sapLength + ", ancestor: " + commonAncestor);
+        assert sapLength == 4;
+        assert commonAncestor == 3;
+
+    }
+}
+
---
+++ Algorithms/Word_net/src/WordNet.java
@@ -0,0 +1,133 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordNet {
+    /*
+     * The WordNet digraph is a rooted DAG : it is acyclic and has one vertex—the root—that is an ancestor of every other vertex.
+     * However, it is not necessarily a tree because a synset can have more than one hypernym.
+     * A small subgraph of the WordNet digraph appears below.
+     */
+    // constructor takes the name of the two input files
+
+    Map<Integer, String> idNounMap;
+    Map<String, List<Integer>> nounMap;
+    Digraph digraph;
+    SAP sap;
+    int count;
+
+    public WordNet(String synsets, String hypernyms) {
+        this.count = 0;
+        readSynsets(synsets);
+        this.digraph = new Digraph(count);
+        readHypernyms(hypernyms);
+
+        if (hasCycle(digraph)) throw new IllegalArgumentException("Digraph has cycle");
+
+        int roots = 0;
+        for (int i = 0; i < count; i++) {
+            if (digraph.outdegree(i) == 0) {
+                roots++;
+            }
+            if (roots > 1) throw new IllegalArgumentException("Digraph contains more than 1 root");
+        }
+
+        sap = new SAP(digraph);
+    }
+
+    // returns all WordNet nouns
+    public Iterable<String> nouns() {
+        return nounMap.keySet();
+    }
+
+    // is the word a WordNet noun?
+    public boolean isNoun(String word) {
+        if (word == null) throw new IllegalArgumentException("Method isNoun(): String word cannot be null!");
+        return nounMap.containsKey(word);
+    }
+
+    // distance between nounA and nounB (defined below)
+    public int distance(String nounA, String nounB) {
+        if (!isNoun(nounA) || !isNoun(nounB))
+            throw new IllegalArgumentException("Method distance(): String nounA / String nounB cannot be null!");
+
+        return sap.length(nounMap.get(nounA), nounMap.get(nounB));
+    }
+
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path (defined below)
+    public String sap(String nounA, String nounB) {
+        if (!isNoun(nounA) || !isNoun(nounB))
+            throw new IllegalArgumentException("Method sap(): String nounA / String nounB cannot be null!");
+        int idAncestorNoun = sap.ancestor(nounMap.get(nounA), nounMap.get(nounB));
+        return idNounMap.get(idAncestorNoun);
+    }
+
+    private void readSynsets(String synsets) {
+        if (synsets == null) throw new IllegalArgumentException("Method readSynsets(): String synsets cannot be null!");
+        In in = new In(synsets);
+        this.idNounMap = new HashMap<>();
+
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            String[] fields = line.split(","); // fields[0] = synset_id, fields[1] = synet, fields[2] = gloss -> not relevant;
+            int id = Integer.parseInt(fields[0]);
+            String[] nouns = fields[1].split(" ");
+            for (String noun : nouns) {
+                this.idNounMap.put(id, noun);
+            }
+            this.count++;
+        }
+    }
+
+    private void readHypernyms(String hypernyms) {
+        if (hypernyms == null) throw new IllegalArgumentException("readHypernyms: String hypernyms cannot be null!");
+        In in = new In(hypernyms);
+        nounMap = new HashMap<>();
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            String[] fields = line.split(",");
+            int nounID = Integer.parseInt(fields[0]);
+            List<Integer> synsetsHypernyms = new ArrayList<>();
+            for (int i = 1; i < fields.length; i++) {
+                int hypernymID = Integer.parseInt(fields[i]);
+                synsetsHypernyms.add(hypernymID);
+                this.digraph.addEdge(nounID, hypernymID);
+            }
+            nounMap.put(idNounMap.get(nounID), synsetsHypernyms);
+        }
+    }
+
+    private boolean hasCycle(Digraph digraph) {
+        boolean[] isVisited = new boolean[digraph.V()];
+        boolean[] isVisiting = new boolean[digraph.V()];
+        for (int i = 0; i < digraph.V(); i++) {
+            if (!isVisited[i] && hasCycle(i, isVisited, isVisiting)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasCycle(int i, boolean[] isVisited, boolean[] isVisiting) {
+        if (isVisited[i]) return false;
+        if (isVisiting[i]) return true;
+        isVisiting[i] = true;
+        for (int adjVertices : digraph.adj(i)) {
+            if (hasCycle(adjVertices, isVisited, isVisiting)) {
+                return true;
+            }
+        }
+        isVisited[i] = true;
+        return false;
+    }
+
+
+    // do unit testing of this class
+    public static void main(String[] args) {
+        String synsets = "https://coursera.cs.princeton.edu/algs4/assignments/wordnet/files/synsets.txt";
+        String hypernyms = "https://coursera.cs.princeton.edu/algs4/assignments/wordnet/files/hypernyms.txt";
+        WordNet wordnet = new WordNet(synsets, hypernyms);
+    }
+}
